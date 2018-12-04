2d3ce799a55e31fcd69bd93abe4b4a8b31f52b0a
PUGE <100284685@qq.com>
2018-12-05 01:01:39 +0800

    使用多线程

--- main.py
+++ main.py
@@ -4,26 +4,33 @@ import string
 import aiohttp
 import asyncio
 import datetime
+import threading
+import requests
 
-async def fetch(session, url):
-  header_dict = {
+def fetch(url):
+  headers = {
     'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; Trident/7.0; rv:11.0) like Gecko',
     'cookie': 'JX_LoginOn=' + ''.join(random.sample(string.ascii_letters + string.digits, 8)) + ';'
   }
-  async with session.get(url, headers=header_dict) as response:
-    return await response.text()
+  r = requests.get(url,headers=headers)
+  print(r.text)
 
-async def main():
+def main():
   # while True:
-  #   async with aiohttp.ClientSession() as session:
-  #     html = await fetch(session, 'http://longquanapi.xuechebu.com/KM2/ClYyAddByMutil?ipaddress=192.168.1.106&ossdk=26&os=an&trainType=3&xxzh=51852038&isJcsdYyMode=5&imei=50E276A23A9DDADF2A9902D1F7AA81CE&appversion=6.2.5.1&params=Z1505.2018%2F12%2F' + day + '.812.&osversion=8.0.0&version=6.2.5.1&jlcbh=')
-  #     print(html)
-  #   time.sleep(1)
+  #   # 获取四天后日期
+  #   today = datetime.date.today() + datetime.timedelta(days=6)
+  #   day = str(today.day)
+  #   t = threading.Thread(target=fetch,args=('http://longquanapi.xuechebu.com/KM2/ClYyAddByMutil?ipaddress=192.168.1.106&ossdk=26&os=an&trainType=3&xxzh=51852038&isJcsdYyMode=5&imei=50E276A23A9DDADF2A9902D1F7AA81CE&appversion=6.2.5.1&params=Z1505.2018%2F12%2F' + day + '.812.&osversion=8.0.0&version=6.2.5.1&jlcbh=',))
+  #   t.start()
+  #   time.sleep(3)
   while True:
     # 获取四天后日期
     today = datetime.date.today() + datetime.timedelta(days=6)
     day = str(today.day)
     print(time.strftime('%Y-%m-%d %H:%M:%S',time.localtime(time.time())))
+
+    url1 = 'http://longquanapi.xuechebu.com/KM2/ClYyAddByMutil?ipaddress=192.168.1.106&ossdk=26&os=an&trainType=3&xxzh=51852038&isJcsdYyMode=5&imei=50E276A23A9DDADF2A9902D1F7AA81CE&appversion=6.2.5.1&params=Z1505.2018%2F12%2F' + day + '.812.&osversion=8.0.0&version=6.2.5.1&jlcbh='
+    url2 = 'http://longquanapi.xuechebu.com/KM2/ClYyAddByMutil?ipaddress=192.168.1.106&ossdk=26&os=an&trainType=3&xxzh=51852038&isJcsdYyMode=5&imei=50E276A23A9DDADF2A9902D1F7AA81CE&appversion=6.2.5.1&params=Z1505.2018%2F12%2F' + day + '.15.&osversion=8.0.0&version=6.2.5.1&jlcbh='
     h = time.strftime("%H")
     if (h == "06" or h == "07"):
       print("进入时间段!")
@@ -35,34 +42,28 @@ async def main():
           if (int(m) >= 59 and int(s) >= 44):
             print('尽全力！')
             print(time.strftime('%Y-%m-%d %H:%M:%S',time.localtime(time.time())))
-            async with aiohttp.ClientSession() as session:
-              html = await fetch(session, 'http://longquanapi.xuechebu.com/KM2/ClYyAddByMutil?ipaddress=192.168.1.106&ossdk=26&os=an&trainType=3&xxzh=51852038&isJcsdYyMode=5&imei=50E276A23A9DDADF2A9902D1F7AA81CE&appversion=6.2.5.1&params=Z1505.2018%2F12%2F' + day + '.812.&osversion=8.0.0&version=6.2.5.1&jlcbh=')
-              print(html)
-            async with aiohttp.ClientSession() as session:
-              html = await fetch(session, 'http://longquanapi.xuechebu.com/KM2/ClYyAddByMutil?ipaddress=192.168.1.106&ossdk=26&os=an&trainType=3&xxzh=51852038&isJcsdYyMode=5&imei=50E276A23A9DDADF2A9902D1F7AA81CE&appversion=6.2.5.1&params=Z1505.2018%2F12%2F' + day + '.15.&osversion=8.0.0&version=6.2.5.1&jlcbh=')
-              print(html)
+            t = threading.Thread(target=fetch,args=(url1,))
+            t.start()
+            t = threading.Thread(target=fetch,args=(url2,))
+            t.start()
             time.sleep(1)
           else:
-            async with aiohttp.ClientSession() as session:
-              html = await fetch(session, 'http://longquanapi.xuechebu.com/KM2/ClYyAddByMutil?ipaddress=192.168.1.106&ossdk=26&os=an&trainType=3&xxzh=51852038&isJcsdYyMode=5&imei=50E276A23A9DDADF2A9902D1F7AA81CE&appversion=6.2.5.1&params=Z1505.2018%2F12%2F' + day + '.812.&osversion=8.0.0&version=6.2.5.1&jlcbh=')
-              print(html)
+            t = threading.Thread(target=fetch,args=(url1,))
+            t.start()
             time.sleep(3)
-            async with aiohttp.ClientSession() as session:
-              html = await fetch(session, 'http://longquanapi.xuechebu.com/KM2/ClYyAddByMutil?ipaddress=192.168.1.106&ossdk=26&os=an&trainType=3&xxzh=51852038&isJcsdYyMode=5&imei=50E276A23A9DDADF2A9902D1F7AA81CE&appversion=6.2.5.1&params=Z1505.2018%2F12%2F' + day + '.15.&osversion=8.0.0&version=6.2.5.1&jlcbh=')
-              print(html)
+            t = threading.Thread(target=fetch,args=(url2,))
+            t.start()
             time.sleep(3)
         else:
           print('准备请求,再等10秒钟!')
           time.sleep(10)
       else:
         if int(m) < 2:
-          async with aiohttp.ClientSession() as session:
-            html = await fetch(session, 'http://longquanapi.xuechebu.com/KM2/ClYyAddByMutil?ipaddress=192.168.1.106&ossdk=26&os=an&trainType=3&xxzh=51852038&isJcsdYyMode=5&imei=50E276A23A9DDADF2A9902D1F7AA81CE&appversion=6.2.5.1&params=Z1505.2018%2F12%2F' + day + '.15.&osversion=8.0.0&version=6.2.5.1&jlcbh=')
-            print(html)
+          t = threading.Thread(target=fetch,args=(url1,))
+          t.start()
           time.sleep(1)
-          async with aiohttp.ClientSession() as session:
-            html = await fetch(session, 'http://longquanapi.xuechebu.com/KM2/ClYyAddByMutil?ipaddress=192.168.1.106&ossdk=26&os=an&trainType=3&xxzh=51852038&isJcsdYyMode=5&imei=50E276A23A9DDADF2A9902D1F7AA81CE&appversion=6.2.5.1&params=Z1505.2018%2F12%2F' + day + '.15.&osversion=8.0.0&version=6.2.5.1&jlcbh=')
-            print(html)
+          t = threading.Thread(target=fetch,args=(url2,))
+          t.start()
           time.sleep(1)
         else:
           print("等待20小时")
@@ -73,5 +74,4 @@ async def main():
   
 
 if __name__ == '__main__':
-  loop = asyncio.get_event_loop()
-  loop.run_until_complete(main())
+  main()
